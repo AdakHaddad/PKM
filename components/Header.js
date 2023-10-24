d891b4e78116d4ce4a10dae6ed3f53e39073ec52
AdakHaddad <muhammadmuqtada@gmail.com>
2023-10-25 06:45:06 +0700

    Responsively

--- components/Header.js
+++ components/Header.js
@@ -11,21 +11,21 @@ import LogoButton from "/public/logo.png"; // Import your logo.png
 
 function Header() {
   return (
-    <header className="bg-transparent py-4 px-10 text-green-400">
-      <div className="flex justify-between items-center">
+    <header className="bg-transparent py-4 px-4 md:px-10 text-green-400">
+      <div className="flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center">
           <Image
             src={LogoButton}
-            width={30} // Set the width as needed
-            height={30} // Set the height as needed
+            width={30}
+            height={30}
             alt="Logo Button"
-            className=" cursor-pointer mr-2 rounded-xl" // Add a cursor-pointer for the button effect
+            className="cursor-pointer rounded-xl"
           />
           <a href="#" className="text-2xl font-sans">
             PsyBot Research
           </a>
         </div>
-        <div className="flex justify-center">
+        <div className="hidden md:flex justify-center">
           <Image
             src="/UGM.png"
             width={100}
@@ -33,9 +33,9 @@ function Header() {
             alt="UGM Logo"
             className="h-9 w-9 mr-2"
           />
-          <Image src={PKM} alt="PKM Logo" className=" h-9 w-16 ml-2" />
+          <Image src={PKM} alt="PKM Logo" className="h-9 w-16 ml-2" />
         </div>
-        <div className="flex space-x-5">
+        <div className="flex space-x-2 md:space-x-5 mt-4 md:mt-0">
           <a
             href="https://www.instagram.com/psybot_pkmugm"
             target="_blank"
--- components/Hero.js
+++ components/Hero.js
@@ -12,25 +12,30 @@ const Hero = () => {
 
   return (
     <section
-      className={`bg-blue-500 text-white h-screen px-10 flex flex-col justify-center items-center ${
+      className={`text-green min-h-screen p-4 md:p-10 flex flex-col justify-center items-center ${
         isFaded ? "opacity-100" : "opacity-0"
       } transition-opacity duration-1000`}
     >
-      <div className="flex justify-center items-center">
-        <div className=" mx-auto text-left">
-          <h1 className="text-5xl font-bold mb-4">
-            PKM Riset Sosio Humaniora UGM
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <div className="text-center md:text-left">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">
+            <span className="hidden md:inline">
+              PKM Riset Sosio Humaniora UGM
+            </span>
+            <span className="md:hidden text-3xl md:text-5xl">PKM RSH UGM</span>
           </h1>
-          <p className="text-xl mb-8">
-            &quot;Efektifitas Kecerdasan Buatan Model Bot Whatsapp sebagai PPP
-            untuk Kondisi Kesepian pada Mahasiswa&quot;
+          <p className="text-lg md:text-xl mb-6 md:mb-8">
+            &quot;Efektifitas Kecerdasan Buatan Model Bot Whatsapp sebagai
+            Pertolongan Pertama Psikologis untuk Kondisi Kesepian pada
+            Mahasiswa&quot;
           </p>
         </div>
         <Image
           src="/positive-mind.png" // Specify the image source
           alt="Positive Mind" // Add a descriptive alt text
-          width={300} // Set the desired width (adjust as needed)
-          height={225} // Set the desired height (adjust as needed)
+          width={250} // Set the desired width for mobile screens
+          height={200} // Set the desired height for mobile screens
+          className="mb-6 md:mb-0 md:flex-row" // Add margin for spacing on mobile
         />
       </div>
       <footer className="mt-8 text-center">
